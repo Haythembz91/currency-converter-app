b3b49a2e61251c9d099f2211cea47ee91dc6d09e
Haythem91 <haylive8@gmail.com>
2024-08-17 23:46:11 +0100

    qsdfqsdf

--- index.cjs
+++ index.cjs
@@ -11,7 +11,7 @@ app.use(cors());
 app.listen(PORT,()=>console.log(`Server is running on port ${PORT}`))
 
 
-app.get('/post',(req,res)=>{
+app.post('/news',(req,res)=>{
     
     const options = {
         method: 'POST',
