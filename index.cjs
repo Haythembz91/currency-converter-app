19ee7fad6d0c7a051786d4953c181715cb7b2814
Haythem91 <haylive8@gmail.com>
2024-08-17 23:07:16 +0100

    Merge branch 'main' of https://github.com/Haythembz91/currency-converter-app

---
+++ index.cjs
@@ -0,0 +1,44 @@
+const PORT = process.env.VITE_SERVER_PORT
+const express = require('express')
+const cors = require('cors')
+const axios = require('axios')
+require('dotenv').config()
+
+const app = express();
+//test
+app.use(cors());
+
+app.listen(PORT,()=>console.log(`Server is running on port ${PORT}`))
+
+
+app.get('/news',(req,res)=>{
+    
+    const options = {
+        method: 'GET',
+        url: 'https://seeking-alpha.p.rapidapi.com/news/v2/list',
+        params: {
+            category: 'market-news::all',
+            size: '10',
+            number: '1'
+        },
+        headers: {
+            'X-RapidAPI-Key': process.env.VITE_RAPID_API_KEY,
+            'X-RapidAPI-Host': 'seeking-alpha.p.rapidapi.com'
+        }
+    }
+    fetch(options).then(response=>{
+        res.json(response.data.data)
+    }).catch(err=>{
+        console.error(err)
+    })
+})
+
+app.get('/currency',(req,res)=>{
+    fetch(process.env.VITE_API_KEY).then(response=>{
+        return response.json()
+    }).then(data=> {
+        res.json(data)
+    }).catch(err=>{
+        console.error(err.message)
+    })
+})
