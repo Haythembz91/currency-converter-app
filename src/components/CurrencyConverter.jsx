c20e5901d9ee2a421833d2abec4cf6a95a6d0641
Haythem91 <haylive8@gmail.com>
2024-08-17 23:20:56 +0100

    LGTM

--- src/components/CurrencyConverter.jsx
+++ src/components/CurrencyConverter.jsx
@@ -16,7 +16,7 @@ const CurrencyConverter = ()=>{
     const currencyExchangeRef = useRef('')
 
 useEffect(()=>{
-    fetch(`${vite.process.VITE_BACKEND_SERVER}/currency`).then(response=>{
+    fetch(`${process.env.VITE_BACKEND_SERVER}/currency`).then(response=>{
         return response.json()
     }).then(data=> {
         setCurrencyList(Object.keys(data['conversion_rates']))
--- src/components/NewsFeed.jsx
+++ src/components/NewsFeed.jsx
@@ -7,7 +7,7 @@ const NewsFeed = ()=>{
 
     const [articles,setArticles]=useState([])
     useEffect(()=>{
-        const url = `${vite.process.VITE_BACKEND_SERVER}/news`;
+        const url = `${process.env.VITE_BACKEND_SERVER}/news`;
         try{
             fetch(url).then(response=>{
             return response.json()
