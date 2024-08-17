8a76f749b8f31d16452498e22dd5b59cb993b692
Haythem91 <haylive8@gmail.com>
2024-08-17 23:27:41 +0100

    qsdf

--- src/components/NewsFeed.jsx
+++ src/components/NewsFeed.jsx
@@ -7,7 +7,7 @@ const NewsFeed = ()=>{
 
     const [articles,setArticles]=useState([])
     useEffect(()=>{
-        const url = `${process.env.VITE_BACKEND_SERVER}/news`;
+        const url = `https://currency-converter-app-p4d5.onrender.com/news`;
         try{
             fetch(url).then(response=>{
             return response.json()
